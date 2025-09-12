1b10966e266fe3c42525d4624344e559103f8ea2
Devansh Gupta <80698697+devgpta19@users.noreply.github.com>
2025-09-12 20:20:34 +0530

    Add files via upload

---
+++ WPT Assignments/Day4/que5.js
@@ -0,0 +1,26 @@
+// <!-- go through object and class syntax in javascript 
+//    (shared files) -->
+
+//using JSON syntax 
+obj={
+fname:"ppp",
+lname:"ggg",
+city:"pune",
+getCity:function()
+{    
+    return this.city
+}
+}
+console.log(obj.city)
+console.log(obj["city"])
+obj.age=10
+console.log(obj)
+
+obj.showInfo = function ()
+{
+    console.log(this.fname,this.city)
+}
+console.log(obj)
+obj.showInfo()
+console.log(obj.getCity())
+
